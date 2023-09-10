002e01abe3e22fca105782325ca3ecdf2abfd3da
Liron Abutbul <110838700+ladunjexa@users.noreply.github.com>
2023-09-10 14:05:04 +0300

    Fix typo

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -22,7 +22,7 @@ const Hero = () => {
         <p className="text-xl font-montserrat text-coral-red">
           Our Summer collections
         </p>
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
             The New Arrival
           </span>
